1485b1633ef4d37db5c52262d744d16b5c90fd1b
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-19 17:19:01 +0900

    implement Text, Append, AppendText, and String methods for Int16 and Uint16 types

---
+++ uint16_test.go
@@ -0,0 +1,67 @@
+package ints
+
+import (
+	"math"
+	"math/big"
+	"strconv"
+	"testing"
+)
+
+func TestUint16_Text(t *testing.T) {
+	var b big.Int
+	for i := range math.MaxUint16 + 1 {
+		a := Uint16(i)
+		b.SetInt64(int64(i))
+		for base := 2; base <= 62; base++ {
+			got := a.Text(base)
+			want := b.Text(base)
+			if got != want {
+				t.Errorf("Uint16(%d).Text(%d) = %q, want %q", i, base, got, want)
+			}
+		}
+	}
+}
+
+func TestUint16_Append(t *testing.T) {
+	var b big.Int
+	var buf []byte
+	for i := range math.MaxUint16 + 1 {
+		a := Uint16(i)
+		b.SetInt64(int64(i))
+		for base := 2; base <= 62; base++ {
+			buf = a.Append(buf[:0], base)
+			got := string(buf)
+			want := b.Text(base)
+			if got != want {
+				t.Errorf("Uint16(%d).Append(buf, %d) = %q, want %q", i, base, got, want)
+			}
+		}
+	}
+}
+
+func TestUint16_AppendText(t *testing.T) {
+	var buf []byte
+	for i := range math.MaxUint16 + 1 {
+		a := Uint16(i)
+		buf, err := a.AppendText(buf[:0])
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := string(buf)
+		want := strconv.FormatInt(int64(i), 10)
+		if got != want {
+			t.Errorf("Uint16(%d).AppendText() = %q, want %q", i, got, want)
+		}
+	}
+}
+
+func TestUint16_String(t *testing.T) {
+	for i := range math.MaxUint16 + 1 {
+		a := Uint16(i)
+		got := a.String()
+		want := strconv.FormatInt(int64(i), 10)
+		if got != want {
+			t.Errorf("Uint16(%d).String() = %q, want %q", i, got, want)
+		}
+	}
+}
